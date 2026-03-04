47d18449e6056688560283a9bc52cbd8432e2ec6
anisnurcahyo6 <anisnurcahyo6@gmail.com>
2026-03-04 14:57:35 +0700

    Update URLGROUP to point to And1.json

--- Andalan/Andalan1.js
+++ Andalan/Andalan1.js
@@ -27,7 +27,7 @@ var Comment18 = 'Andalan1';
 
 
 
-var URLGROUP = `https://raw.githubusercontent.com/anisnurcahyo6/E11RRD33K44B00/refs/heads/main/Comment/${Comment18}.json`;
+var URLGROUP = `https://raw.githubusercontent.com/anisnurcahyo6/E11RRD33K44B00/refs/heads/main/Comment/And1.json`;
 var SCRIPT_NAME = Comment18
 var refresh = 40;
 var URLADMIN = "https://raw.githubusercontent.com/anisnurcahyo6/E11RRD33K44B00/refs/heads/main/Admin.json"
