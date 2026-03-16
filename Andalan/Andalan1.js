69f399175e201cd67abc4f8ab9f0f337a332fd3e
anisnurcahyo6 <anisnurcahyo6@gmail.com>
2026-03-16 22:47:25 +0700

    Update URLGROUP to new GitHub repository

--- Andalan/Andalan1.js
+++ Andalan/Andalan1.js
@@ -27,7 +27,7 @@ var Comment18 = 'And1';
 
 
 
-var URLGROUP = `https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Comment/${Comment18}.json`;
+var URLGROUP = `https://raw.githubusercontent.com/anisnurcahyo6/E11RRD33K44B00/refs/heads/main/Comment/${Comment18}.json`;
 var SCRIPT_NAME = Comment18
 var refresh = 20;
 var URLADMIN = "https://raw.githubusercontent.com/natasyabimosakti/ADMIN/refs/heads/main/Admin_Jovi_123.json"
