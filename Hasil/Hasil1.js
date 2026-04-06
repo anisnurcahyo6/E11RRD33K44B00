142e189296470f45d5be4260232155273b47e593
anisnurcahyo6 <anisnurcahyo6@gmail.com>
2026-04-06 23:00:35 +0700

    Update Hasil1.js

--- Hasil/Hasil1.js
+++ Hasil/Hasil1.js
@@ -24,7 +24,7 @@ var Comment18 = 'Has1';
 
 
 
-var URLGROUP = `https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Comment/${Comment18}.json`;
+var URLGROUP = `https://raw.githubusercontent.com/anisnurcahyo6/E11RRD33K44B00/refs/heads/main/Comment/${Comment18}.json`;
 var SCRIPT_NAME = Comment18
 var refresh = 20;
 var URLADMIN = "https://raw.githubusercontent.com/natasyabimosakti/ADMIN/refs/heads/main/Admin_group_Baru.json"
