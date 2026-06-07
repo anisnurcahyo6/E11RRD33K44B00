f1b059b6e5569d9f1d209d204bc7d44b058ef731
anisnurcahyo6 <anisnurcahyo6@gmail.com>
2026-06-07 15:28:18 +0700

    Update Hasil1.js

--- Hasil/Hasil1.js
+++ Hasil/Hasil1.js
@@ -1,5 +1,25 @@
-// babon_core.js
-window.initBabonLogic = function (namagroup18, Comment18) {
+// ==UserScript==
+// @name         hasil 1
+// @namespace    http://tampermonkey.net/
+// @version      7.1
+// @description  try to take over the world!
+// @updateURL    https://raw.githubusercontent.com/anisnurcahyo6/E11RRD33K44B00/refs/heads/main/Hasil/Hasil1.js
+// @downloadURL  https://raw.githubusercontent.com/anisnurcahyo6/E11RRD33K44B00/refs/heads/main/Hasil/Hasil1.js
+// @author       You
+// @match        http*://*/*
+// @run-at       document-end
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        GM.setValue
+// @grant        GM.getValue
+// @grant        window.close
+// @grant        GM_xmlhttpRequest
+// @connect      api.telegram.org
+// @connect      raw.githubusercontent.com
+// @connect      localhost
+// ==/UserScript==
+
+var namagroup18 = 'Jawatengah';
+var Comment18 = 'Has1';
 
 
     // --- 1. ANTI-THROTTLE & KEEP-ALIVE (Solusi Tab Background) ---
